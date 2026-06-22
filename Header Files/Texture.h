9ba0f8d2b46a6eeb5b47fc9258d94565eb8eaf65
Tajinderpal Toor <tajinderpal1999@gmail.com>
2026-06-22 19:28:51 -0400

    added texture and camera and 3d

--- Header Files/Texture.h
+++ Header Files/Texture.h
@@ -16,6 +16,13 @@ class Texture {
     public:
     GLuint ID;
     GLenum type;
-    Texture (const char* image, GLenum texType, GLenum slot, GLenum format, GLenum 
+    Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType);
+    void texUnit(Shader& shader, const char* uniform, GLuint unit);
+
+    void Bind();
+    void Unbind();
+    void Delete();
+
+
 };
 #endif
